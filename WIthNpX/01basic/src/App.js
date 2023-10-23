ea39ae10bc02502b9e1e62ac21757755c496f49b
Bhavin <patelbc94@gmail.com>
2023-10-23 11:58:01 +0530

    new reac and react with vite and custom react

---
+++ WIthNpX/01basic/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Hello Bhavin Patel</h1>;
+}
+
+export default App;
---
+++ vite/viteReact/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>React With Vite</h1>
+    </>
+  );
+}
+
+export default App;
